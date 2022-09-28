1c69d3b1f95f1d01f3aa7b88f59c2d8f033c9291
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2022-09-28 15:53:17 -0300

    :bulb:  Add end-to-end testing and code coverage

---
+++ m1-javascript-testing/04-coverage-e2e/src/api.test.js
@@ -0,0 +1,53 @@
+const { describe, it } = require('mocha');
+const request = require('supertest');
+const assert = require('assert');
+
+const app = require('./api');
+
+describe('API Suite test', () => {
+  describe('/contact', () => {
+    it('should request the contact page and return HTTP Status 200', async () => {
+      const response = await request(app)
+        .get('/contact')
+        .expect(200);
+
+      // console.log('response', response);
+
+      assert.deepStrictEqual(response.text, 'contact us page');
+    });
+  });
+
+  describe('/hello', () => {
+    it('should request an inexistent rout /hi and redirect to /hello', async () => {
+      const response = await request(app)
+        .get('/hi')
+        .expect(200);
+
+      assert.deepStrictEqual(response.text, 'Hello, world!');
+    });
+
+    describe('/login', () => {
+      it('should login successfully on the login route and return HTTP Status 200', async () => {
+        const response = await request(app)
+          .post('/login')
+          .send({ username: 'yvesguilherme', password: '123' })
+          .expect(200);
+
+        assert.deepStrictEqual(response.text, 'Login has succeeded!');
+      });
+
+      it('should unauthorize a request when requesting it using wrong credentials and return HTTP Status 401', async () => {
+        const response = await request(app)
+          .post('/login')
+          .send({ username: 'forabozo', password: '123' })
+          .expect(401);
+
+        // console.log('response-unauthorized', response.unauthorized);
+
+        assert.ok(response.unauthorized);
+        assert.deepStrictEqual(response.text, 'Logging failed!');
+
+      });
+    });
+  });
+});
