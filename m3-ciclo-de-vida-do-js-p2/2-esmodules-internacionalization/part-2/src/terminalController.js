c5bad67ac555ffba2f152c964bffa2c0eb1e53d1
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2022-10-31 22:00:14 -0300

    :recycle:  Add code refactoring and core system

---
+++ m3-ciclo-de-vida-do-js-p2/2-esmodules-internacionalization/part-2/src/terminalController.js
@@ -0,0 +1,56 @@
+import DraftLog from 'draftlog';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import readline from 'readline';
+
+import Person from './person.js';
+
+export default class TerminalController {
+  constructor() {
+    this.print = {};
+    this.data = {};
+  }
+
+  initializeTerminal(database, language) {
+    DraftLog(console).addLineListener(process.stdin);
+    this.terminal = readline.Interface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    this.initializeTable(database, language);
+  }
+
+  initializeTable(database, language) {
+    const data = database.map(item => new Person(item).formatted(language));
+    const table = chalkTable(this.getTableOptions(), data);
+
+    this.print = console.draft(table);
+    this.data = data;
+  }
+
+  question(msg = 'What\'\s your name?') {
+    return new Promise(resolve => this.terminal.question(msg, resolve));
+  }
+
+  closeTerminal() {
+    this.terminal.close();
+  }
+
+  getTableOptions() {
+    return {
+      leftPad: 2,
+      /**
+       * Field: campo no json;
+       * Name:  nome na tabela.
+       */
+      columns: [
+        { field: 'id', name: chalk.cyan('ID') },
+        { field: 'vehicles', name: chalk.magenta('Vehicles') },
+        { field: 'kmTraveled', name: chalk.redBright('Km Traveled') },
+        { field: 'from', name: chalk.greenBright('From') },
+        { field: 'to', name: chalk.yellowBright('To') }
+      ]
+    };
+  }
+}
