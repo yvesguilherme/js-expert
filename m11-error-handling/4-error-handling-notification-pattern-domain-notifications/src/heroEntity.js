d719d93fd6607eabad8b7dde54a8f1996d4489e5
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-07-31 23:51:53 -0300

    :bulb:  Adding notification pattern and domain notifications

---
+++ m11-error-handling/4-error-handling-notification-pattern-domain-notifications/src/heroEntity.js
@@ -0,0 +1,21 @@
+import NotificationContext from "./notificationContext.js";
+
+export default class HeroEntity extends NotificationContext {
+  constructor({ name, age }) {
+    super();
+    this.name = name;
+    this.age = age;
+  }
+
+  isValid() {
+    if (this.age < 20) {
+      this.addNotification('age must be higher than 20!');
+    }
+
+    if (this.name?.length < 4) {
+      this.addNotification('name length must be higher than 4!');
+    }
+
+    return !this.hasNotifications();
+  }
+}
---
+++ m11-error-handling/4-error-handling-notification-pattern-domain-notifications/src/notificationContext.js
@@ -0,0 +1,13 @@
+export default class NotificationContext {
+  constructor() {
+    this.notifications = [];
+  }
+
+  hasNotifications() {
+    return this.notifications.length > 0;
+  }
+
+  addNotification(notification) {
+    this.notifications.push(notification);
+  }
+}
